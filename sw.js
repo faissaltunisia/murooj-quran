5eae273d149bce35ccf0eb91744ecfa1929b0c32
SalalahSchool <aribifaissal@gmail.com>
2025-12-19 19:11:47 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,35 +1,23 @@
-const cacheName = 'murooj-v4'; // قمنا بتغيير الإصدار لتنظيف الكاش القديم
-const assets = ['./', './index.html', './manifest.json'];
+const cacheName = 'murooj-v4';
+const assets = ['./', './index.html'];
 
-// عند التثبيت: تخزين الملفات الأساسية
 self.addEventListener('install', e => {
-  self.skipWaiting(); // إجبار المتصفح على تفعيل النسخة الجديدة فوراً
-  e.waitUntil(
-    caches.open(cacheName).then(c => c.addAll(assets))
-  );
+  self.skipWaiting();
+  e.waitUntil(caches.open(cacheName).then(c => c.addAll(assets)));
 });
 
-// عند التفعيل: حذف الكاش القديم تماماً
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== cacheName).map(key => caches.delete(key))
-      );
-    })
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(
+    keys.filter(k => k !== cacheName).map(k => caches.delete(k))
+  )));
 });
 
-// استراتيجية Network First: اطلب من الإنترنت أولاً، وإذا فشل استخدم الكاش
 self.addEventListener('fetch', e => {
   e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        // إذا نجح الاتصال، خذ نسخة حديثة للكاش ورجع النتيجة
-        const resClone = res.clone();
-        caches.open(cacheName).then(cache => cache.put(e.request, resClone));
-        return res;
-      })
-      .catch(() => caches.match(e.request)) // إذا انقطع الإنترنت، اعرض المخزن
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(cacheName).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
   );
 });
